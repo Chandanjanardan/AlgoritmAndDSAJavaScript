f3a6a16526bbd58e35c86bc99b1a5ca9bcff4e71
Chandan Janardan <chandanjanardan@gmail.com>
2023-01-30 03:25:34 +0530

    adding prime number

---
+++ primeNumber.js
@@ -0,0 +1,14 @@
+const primeNumber=(n)=>{
+    if (n<2){
+    return false
+}
+for (let i=2;i<n;i++){
+    if (n%i === 0){
+        return `${n} is not a prime number`
+    }else{
+        return `${n} is a prime number`
+        
+    }
+}
+}
+console.log(primeNumber(5))
