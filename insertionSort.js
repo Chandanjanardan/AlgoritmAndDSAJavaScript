315729fd55665393c4b070cdc0927882f0463803
Chandan Janardan <chandanjanardan@gmail.com>
2023-02-04 12:59:29 +0530

    adding insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr){
+    for (let i=1;i<arr.length;i++){
+        let numberToInsert=arr[i]
+        let j= i-1;
+        while (j>=0 && arr[j]>numberToInsert){
+            arr[j+1] = arr[j]
+            j=j-1
+        }
+        arr[j+1] = numberToInsert
+    }
+}
+
+arr=[8,4,3,6,60,5,2,1]
+insertionSort(arr)
+console.log(arr)
+console.log("insertion sort")
