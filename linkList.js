1cdd5ce95bb21bda74d75a219a4305d94e708e87
Chandan Janardan <chandanjanardan@gmail.com>
2023-02-08 19:57:02 +0530

    adding prepend and print in linklist

--- linkList.js
+++ linkList.js
@@ -1,4 +1,53 @@
 // Random access is not fesible 
 // insertion at begining deleate and search
+class Node{
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+class LinkedList {
+    constructor(){
+        this.head=null; 
+        this.size=0
+    }
+    isEmpty(){
+        return this.size ===0
+    }
+    getSize(){
+        return this.size
+    }
+    prepend(value){
+        const node =new Node(value)
+        if(this.isEmpty()){
+            this.head= node
+        }else{
+            node.next=this.head
+            this.head=node
+        }
+        this.size++
+    }
+    print(){
+        if(this.isEmpty()){
+            console.log("List is Empty")
+        }else{
+            let curr=this.head
+            let listVlaue =" "
+            while(curr){
+                listVlaue+=`${ curr.value }`
+                curr=curr.next
+            }
+            console.log(listVlaue)
+        }
+    }
+}
 
-console.log("linklist")
+const list =new LinkedList()
+console.log("list is empty?",list.isEmpty())
+console.log("list size",list.getSize())
+list.print()
+list.prepend(10)
+list.print()
+list.prepend(20)
+list.prepend(30)
+list.print()
